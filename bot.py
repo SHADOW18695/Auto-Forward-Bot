0f791ad39db4d9884c9c01d852c2897d034802cd
SHADOW18695 <99009597+SHADOW18695@users.noreply.github.com>
2024-04-19 18:37:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ class channelforward(pyrogram.Client):
     
     def __init__(self):
         super().__init__(
+            name="CHANNELFORWARD",
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
